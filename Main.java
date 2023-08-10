0982c0fed148546fd72523c419bbc8612b63b446
Chaitanyamore26 <141562661+Chaitanyamore26@users.noreply.github.com>
2023-08-10 22:28:47 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("THE SUM OF THESE NUMBERS IS:  ");
+        int num1 = 12;
+        int num2 = 43;
+        int num3 = 27;
+        int num4 = 54;
+        int sum = num1 + num2 + num3 + num4;
+        System.out.println(sum);
+        System.out.println("Hello world!");
+    }
+}
+
+
+
